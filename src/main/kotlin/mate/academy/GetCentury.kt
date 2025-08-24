81046408581eeef4ca2d0231aa0e067bf5951918
FroGitHub <andrey8102310@gmail.com>
2025-08-24 20:41:42 +0200

    solving

--- src/main/kotlin/mate/academy/GetCentury.kt
+++ src/main/kotlin/mate/academy/GetCentury.kt
@@ -1,5 +1,10 @@
 package mate.academy
 
+const val YEARS_IN_CENTURY = 100
+const val NO_EXTRA_CENTURY = 0
+const val NEXT_CENTURY = 1
+
 fun getCentury(year: Int) : Int {
-    return year / 100 + if (year % 100 == 0) 0 else 1
+    return year / YEARS_IN_CENTURY +
+            if (year % YEARS_IN_CENTURY == 0) NO_EXTRA_CENTURY else NEXT_CENTURY
 }
